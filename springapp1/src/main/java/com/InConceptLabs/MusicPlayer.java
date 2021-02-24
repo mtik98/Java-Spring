eee79f5a92ce771c0d61c636b0299044559c5660
= <=>
2021-02-25 02:00:41 +0400

    Spring

--- springapp1/src/main/java/com/InConceptLabs/MusicPlayer.java
+++ springapp1/src/main/java/com/InConceptLabs/MusicPlayer.java
@@ -2,31 +2,38 @@ package com.InConceptLabs;
 
 public class MusicPlayer {
     private Music music;
-    private String name;
-    int volume;
 
-    public MusicPlayer(Music music) { this.music = music; }
+    private String name;
+    private int volume;
+
+    //Ioc
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
     public MusicPlayer() {}
 
-    public void setMusic(Music music) { this.music = music; }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public int getVolume() {
         return volume;
     }
 
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic() {
-        System.out.println("playing " + music.getSong());
+        System.out.println("Playing: " + music.getSong());
     }
 }
--- springapp1/src/main/java/com/InConceptLabs/TestSpring.java
+++ springapp1/src/main/java/com/InConceptLabs/TestSpring.java
@@ -4,12 +4,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext
-                ("applicationContext.xml");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
+
         MusicPlayer musicPlayer = applicationContext.getBean("musicPlayer", MusicPlayer.class);
+
         musicPlayer.playMusic();
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+
         applicationContext.close();
     }
 }
